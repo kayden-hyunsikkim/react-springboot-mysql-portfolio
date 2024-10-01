596f370e736cff9e16baaead72a16b0790c67803
Kayden Hyunsik kim <117362973+kayden-hyunsikkim@users.noreply.github.com>
2024-10-01 11:47:19 +0930

    Update index.tsx

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from 'react'; 
+import { createRoot } from 'react-dom/client'; 
+import { Provider } from 'react-redux'; 
+import { bindActionCreators } from 'redux'; 
 
-import getStore from 'app/config/store';
-import { registerLocale } from 'app/config/translation';
-import setupAxiosInterceptors from 'app/config/axios-interceptor';
-import { clearAuthentication } from 'app/shared/reducers/authentication';
-import ErrorBoundary from 'app/shared/error/error-boundary';
+import getStore from 'app/config/store'; 
+import { registerLocale } from 'app/config/translation'; 
+import setupAxiosInterceptors from 'app/config/axios-interceptor'; //for handling errors about authebtication
+import { clearAuthentication } from 'app/shared/reducers/authentication'; //for clear authentication when user log out
+import ErrorBoundary from 'app/shared/error/error-boundary'; // for error handing which occur in application
 import AppComponent from 'app/app';
 import { loadIcons } from 'app/config/icon-loader';
 
